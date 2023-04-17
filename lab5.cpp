7f425952b53aa98e0a6364d896002626e3f8e1a4
Shvidky Bogdan <bogdunya@mail.ru>
2023-04-17 16:15:44 +0300

    ddddd_1

--- lab5.cpp
+++ lab5.cpp
@@ -20,7 +20,7 @@ int main() {
         cout << "Roots are real and different." << endl;
         cout << "Root 1 = " << root1 << endl;
         cout << "Root 2 = " << root2 << endl;
-        // main 4
+        // main 4: change 23
     } else if (discriminant == 0) {
             // main 5
         root1 = -b / (2 * a);
